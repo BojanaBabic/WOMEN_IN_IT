93e1ae8ce5a495a78d488b7f768d04bba21fd2ec
BojanaBabic <babicbojana85@gmail.com>
2019-02-02 12:06:58 +0100

    second change

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
   createCanvas(400, 400);
+  background(200);
+
+  strokeWeight(4);
+
+  fill(0,170,0);
+  rect(width/2-50, height/2-50, 100, 100);
 }
 
- function draw() {
-  background(100);
-}
+
+
